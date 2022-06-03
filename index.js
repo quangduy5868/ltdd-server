7d0fcf06408675152def5365d44186f0f838bae8
Nguyen Quang Duy <quangduynguyen@deevotech.com>
2022-06-03 13:16:12 +0700

    update

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
-var express = require('express')
-var app = express()
+var express = require("express");
+var app = express();
+const bodyParser = require("body-parser");
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+const { Pool } = require("pg");
+require("dotenv").config();
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+const isProduction = process.env.NODE_ENV === "production";
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+app.use(bodyParser.json());
+app.set("port", process.env.PORT || 3000);
+
+app.get("/", (request, response) => {
+  pool.query(`SELECT * FROM Scores;`, (err, res) => {
+    if (err) {
+      console.log("Error - Failed to select all from Scores");
+      console.log(err);
+      response.end();
+    } else {
+      response.json(res?.rows);
+      response.end();
+    }
+  });
+});
+
+app.post("/", (request, response) => {
+  const { username, point, game } = request.body;
+
+  pool.query(
+    `INSERT INTO Scores(username, point, game) VALUES ($1, $2, $3)`,
+    [username, point, game],
+    (err, res) => {
+      if (err) {
+        console.log("Error - Fail to insert to Scores");
+        console.log(err);
+      } else {
+        console.log("Score Inserted!");
+      }
+    }
+  );
+
+  response.json({ username, point, game });
+  response.end();
+});
+
+app.listen(app.get("port"), () => {
+  console.log("Node app is running at localhost:" + app.get("port"));
+  pool.query(`SELECT * FROM Scores;`, (err, res) => {
+    if (err) {
+      console.log("Error - Failed to select all from Scores");
+      console.log(err);
+    } else {
+      console.log("Connect to database successfully!");
+    }
+  });
+});
